1c9d6d8fbc2d5efa0a9e205edb3637965890f329
Christian Gilbertson <cjgilbe@sandia.gov>
2023-11-29 15:32:54 -0700

    placeholder for empty alt string

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -90,6 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+                        alt="empty alt string"
                       />
                       <p>{invoice.name}</p>
                     </div>
